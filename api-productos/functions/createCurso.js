bed4d248b4b76f7ba19686ebc107d946e0acf0d7
Davidht77 <davidandreshuette@gmail.com>
2025-07-07 23:44:05 -0500

    ninety

--- api-productos/functions/createCurso.js
+++ api-productos/functions/createCurso.js
@@ -8,18 +8,18 @@ const TABLE_NAME = process.env.CURSOS_TABLE_NAME;
 module.exports.createCurso = async (event) => {
   try {
   const body = JSON.parse(event.body);
-  const { tenant_id, nombre, descripcion, duracion, imagen_url, categories, precio } = body;
+  const { tenant_id, nombre, descripcion, duracion, imagen_url, categories, precio, rating } = body;
   const curso_id = uuidv4();
 
   // Validar que los campos requeridos existan
-  if (!tenant_id || !nombre || !descripcion || !duracion || !imagen_url || !categories || !Array.isArray(categories) || categories.length === 0 || precio === undefined) {
+  if (!tenant_id || !nombre || !descripcion || !duracion || !imagen_url || !categories || !Array.isArray(categories) || categories.length === 0 || precio === undefined || rating === undefined) {
     return {
       statusCode: 400,
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        message: 'Faltan campos requeridos. Asegúrate de proporcionar tenant_id, nombre, descripcion, duracion, imagen_url, categories (como un array no vacío) y precio.',
+        message: 'Faltan campos requeridos. Asegúrate de proporcionar tenant_id, nombre, descripcion, duracion, imagen_url, categories (como un array no vacío), precio y rating.',
       }),
     };
   }
@@ -58,6 +58,7 @@ module.exports.createCurso = async (event) => {
         imagen_url: imagen_url,
         categories: categories,
         precio: precio,
+        rating: rating,
       },
     };
 
